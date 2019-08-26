bc6b5a57ff00b1639712b9db76e0300dc6cfa2be
kuro525 <deep.impact525-2012-5-pc@hotmail.co.jp>
2019-08-27 00:10:43 +0900

    [Fix] リソースベースルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #
   # root 'boards#index'
-  get 'boards' => 'boards#index'
-  get 'boards/new' => 'boards#new'
-  post 'boards' => 'boards#create'
-  get 'boards/:id' => 'boards#show'
+  # get 'boards' => 'boards#index'
+  # get 'boards/new' => 'boards#new'
+  # post 'boards' => 'boards#create'
+  # get 'boards/:id' => 'boards#show'
 
+  resources :boards, onry: [:index, :new, :create, :show]
 end
